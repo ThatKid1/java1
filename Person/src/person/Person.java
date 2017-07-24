56573a27f689a3271357a888a8b39b82bb1d6fbd
ThatKid1 <iamalxzander@gmail.com>
2017-07-24 10:43:15 -0700

    person

---
+++ Person/src/person/Person.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class Person {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // habbida habbida
+        System.out.println("Alex Tejeda");
+        System.out.println("14");
+        System.out.println("Las Vegas");
+        System.out.println("One of my hobbies is playing video games");
+        System.out.println("Red");
+    }
+    
+}
