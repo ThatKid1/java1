87f971d8ea0d0fe7240ef31cafd2ebcdc5ec4344
ThatKid1 <iamalxzander@gmail.com>
2017-07-24 19:18:08 -0700

    doubles

--- Doubles/src/doubles/Doubles.java
+++ Doubles/src/doubles/Doubles.java
@@ -1,16 +1,23 @@
 package doubles;
+
 import java.util.Scanner;
+
 public class Doubles {
+
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner (System.in);
-        int n1 = (int) (Math.random() * 6);
+        Scanner keyboard = new Scanner(System.in);
+        int n1 = (int) (Math.random() * 6)+1;
         System.out.println(n1);
-        int n2 = (int) (Math.random() * 6);
-        System.out.println(n1);
-       
-       while (n1!=n2) {
-           System.out.println();
-    }
-    
+        int n2 = (int) (Math.random() * 6)+1;
+        System.out.println(n2);
+
+        while (n1 != n2) {
+            System.out.println();
+            n1 = (int) (Math.random() * 6)+1;
+            System.out.println(n1);
+            n2 = (int) (Math.random() * 6)+1;
+            System.out.println(n2);
+        }
+        System.out.println("Congrats!");
     }
 }
